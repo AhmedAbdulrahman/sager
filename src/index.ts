68dbf676acaf3581040ead71dd1970520315cb87
Ahmed Abdulrahman <a.kasapbashi@live.com>
2022-04-18 14:23:49 +0200

    chore(core): setup apollo server

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { ApolloServer } from 'apollo-server-express'
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
+import express from 'express'
+import http from 'http'
+
+const main = async () => {
+  // Required logic for integrating with Express
+  const app = express()
+  const httpServer = http.createServer(app)
+
+  // ApolloServer initialization, plus the drain plugin.
+  const server = new ApolloServer({
+    typeDefs: [],
+    resolvers: [],
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  })
+
+  // More required logic for integrating with Express
+  await server.start()
+
+  server.applyMiddleware({
+    app,
+
+    // By default, apollo-server hosts its GraphQL endpoint at the
+    // server root. However, *other* Apollo Server packages host it at
+    // /graphql. Optionally provide this to match apollo-server.
+    path: '/',
+  })
+
+  // Server startup
+  await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve))
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+}
+
+main().catch((err) => {
+  console.error(err)
+})
